2e2282a1381e43e9bffbc11e334c8b7790b9c170
d-a-weston <daniel.aaron.weston@gmail.com>
2023-05-30 20:53:44 +1000

    feat: create header for app

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header: React.FC = () => {
+  return (
+    <div className="navbar bg-primary text-primary-content">
+      <div className="flex-1 px-5 text-3xl font-bold">Name Fetch</div>
+    </div>
+  );
+};
